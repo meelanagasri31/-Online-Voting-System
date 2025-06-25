3c5bef8076a4e8a4a5dbe3cd4e95eae5ad10365a
meelanagasri31 <meelanagasri@gmail.com>
2025-06-25 14:28:06 +0530

    Create DBConnection.java

---
+++ DBConnection.java
@@ -0,0 +1,11 @@
+package com.voting;
+
+import java.sql.*;
+
+public class DBConnection {
+    public static Connection getConnection() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/voting_system", "root", "your_password");
+    }
+}
